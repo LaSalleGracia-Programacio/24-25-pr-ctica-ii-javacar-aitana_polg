711f659ee9485e890858cf60b9c90a5d55d3d600
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-18 11:42:01 +0100

    Añado getter

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -3,4 +3,13 @@ package org.JavaCar;
 public class Roda {
     String marca;
     double diametre;
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getDiametre() {
+        return diametre;
+    }
 }
