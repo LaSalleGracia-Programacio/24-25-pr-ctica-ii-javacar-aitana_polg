c779b6b142b4ff523b9b0debc3556ba130a858d5
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-18 11:42:59 +0100

    Añado constructor

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -4,6 +4,10 @@ public class Roda {
     String marca;
     double diametre;
 
+    public Roda(String marca, double diametre) {
+        this.marca = marca;
+        this.diametre = diametre;
+    }
 
     public String getMarca() {
         return marca;
