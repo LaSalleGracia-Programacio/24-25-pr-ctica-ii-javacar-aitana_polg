a68fd06cd2cb0d1f5ba76b82a38c90074e14d30b
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-18 11:48:31 +0100

    Añado getters y el constructor

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -1,6 +1,23 @@
 package org.JavaCar;
 
 public class Motor {
+    //inici VARIABLES
     String tipus;
     int potencia;
+    //Fi VARIABLES
+
+    //Constructor
+    public Motor(String tipus, int potencia) {
+        this.tipus = tipus;
+        this.potencia = potencia;
+    }
+
+    //GETTERS
+    public String getTipus() {
+        return tipus;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
 }
