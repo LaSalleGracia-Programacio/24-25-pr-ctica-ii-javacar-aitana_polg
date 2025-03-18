64055b3efe4f7fb7c4ee77991aa368f9e0558538
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-18 11:39:40 +0100

    añado getter

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -6,4 +6,9 @@ public class Moto extends Vehicle implements Llogable{
     public Moto(String matricula, String marca, String model, double preuBase, String motor, int rodes, String etiquetaAmbiental) {
         super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
     }
+
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
 }
