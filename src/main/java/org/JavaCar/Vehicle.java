2d8ec106bfa45ec56cca89d904e18dd526f2ae83
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-18 10:45:36 +0100

    Añado variables, constructor y getters

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,4 +1,51 @@
 package org.JavaCar;
 
-public class Vehicle {
+public abstract class Vehicle {
+    //inici VARIABLES
+    protected String matricula;
+    protected String marca;
+    protected String model;
+    protected double preuBase;
+    protected String motor;
+    protected int rodes;
+    protected String etiquetaAmbiental;
+    //Fi VARIABLES
+
+    public Vehicle(String matricula, String marca, String model, double preuBase, String motor, int rodes, String etiquetaAmbiental) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.motor = motor;
+        this.rodes = rodes;
+        this.etiquetaAmbiental = etiquetaAmbiental;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPreuBase() {
+        return preuBase;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public int getRodes() {
+        return rodes;
+    }
+
+    public String getEtiquetaAmbiental() {
+        return etiquetaAmbiental;
+    }
 }
