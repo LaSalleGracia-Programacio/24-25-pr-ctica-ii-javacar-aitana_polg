697b6508c696c4520eff86acdbbdd18faf866f64
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-21 13:34:30 +0100

    EtiquetaAmbiental borrada

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Cotxe extends Vehicle implements Llogable{
     int nombrePlaces;
 
-    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces,Motor motor, ArrayList<Roda> rodes, EtiquetaAmbiental etiquetaAmbiental) {
-        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces,Motor motor, ArrayList<Roda> rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes);
         this.nombrePlaces = nombrePlaces;
     }
 }
--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Moto extends Vehicle implements Llogable{
     int cilindrada;
 
-    public Moto(String matricula, String marca, String model, double preuBase, Motor motor, ArrayList<Roda> rodes, EtiquetaAmbiental etiquetaAmbiental) {
-        super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+    public Moto(String matricula, String marca, String model, double preuBase, Motor motor, ArrayList<Roda> rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes);
     }
 
 
