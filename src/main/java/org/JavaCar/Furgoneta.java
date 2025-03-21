343f16617ece2997564af7e4d2c85772ea563a16
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-21 13:18:58 +0100

    Furgoneta capacidad

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
 public class Furgoneta extends Vehicle implements Llogable{
     double capacitatCarga;
 
-    public Furgoneta(String matricula, String marca, String model, double preuBase, Motor motor, ArrayList<Roda> rodes, EtiquetaAmbiental etiquetaAmbiental) {
+    public Furgoneta(String matricula, String marca, String model, double preuBase, double capacitatCarga, Motor motor, ArrayList<Roda> rodes, EtiquetaAmbiental etiquetaAmbiental) {
         super(matricula, marca, model, preuBase, motor, rodes, etiquetaAmbiental);
+
+        this.capacitatCarga = capacitatCarga;
+    }
+
+    public double getCapacitatCarga() {
+        return capacitatCarga;
     }
 }
