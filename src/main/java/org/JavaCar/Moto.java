ecf35cdb87902d2e6f863f00dfc80f901ba567ec
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-23 14:02:31 +0100

    Modifico modificador cilindrada

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Moto extends Vehicle implements Llogable{
-    int cilindrada;
+    private int cilindrada;
 
     public Moto(String matricula, String marca, String model, double preuBase,int cilindrada, Motor motor, Roda[] rodesArray) {
         super(matricula, marca, model, preuBase, motor, new ArrayList<>(Arrays.asList(rodesArray)));
