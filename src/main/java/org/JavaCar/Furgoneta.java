0b031fc979447f6ecd8911659b575d327c867b21
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-23 14:00:36 +0100

    Modifico modif variable capaciadadCarga

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Furgoneta extends Vehicle implements Llogable{
-    double capacitatCarga;
+    private double capacitatCarga;
 
     public Furgoneta(String matricula, String marca, String model, double preuBase, double capacitatCarga, Motor motor, Roda[] rodesArray) {
         super(matricula, marca, model, preuBase, motor, new ArrayList<>(Arrays.asList(rodesArray)));
