dbbd474ac923fba105cde786f76466e66b44d0a8
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-23 14:17:22 +0100

    diametre private

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -2,7 +2,7 @@ package org.JavaCar;
 
 public class Roda {
     private String marca;
-    double diametre;
+    private double diametre;
 
     public Roda(String marca, double diametre) {
         this.marca = marca;
