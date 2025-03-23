528cc5b5be0a5b76cb1bf2bcca8365e8f9e4f6b1
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-23 13:35:06 +0100

    comentari

--- src/main/java/org/JavaCar/GestorLloguers.java
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -3,7 +3,7 @@ package org.JavaCar;
 import java.util.List;
 
 public class GestorLloguers {
-    int dies; // Esto esta relacionado con el int dies de Llogable
+    int dies; // Nombre de dies que està llogat (Llogable)
     double preuMax;
 
     public static double calcularIngressosTotals(List<Vehicle> vehicles, int dies){
