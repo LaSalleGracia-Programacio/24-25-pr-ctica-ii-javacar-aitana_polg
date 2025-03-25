9ac6a63e7d11bd89b7c3852d8f23315c5a7a6cd5
aitana54 <aitana.buiza@gracia.lasalle.cat>
2025-03-25 20:59:14 +0100

    Cometario tipos de tiquetas

--- src/main/java/org/JavaCar/EtiquetaAmbiental.java
+++ src/main/java/org/JavaCar/EtiquetaAmbiental.java
@@ -1,5 +1,9 @@
 package org.JavaCar;
 
 public enum EtiquetaAmbiental {
-    B, C, Eco, ZeroEmisions, SenseEtiqueta
+    B, // Dièsel o gasolina amb emissions moderades
+    C, // Gasolina o dièsel més antic
+    Eco, // Vehicles híbrids o que compleixen amb estàndards ecològics
+    ZeroEmisions, // Vehicles elèctrics o de baixes emissions
+    SenseEtiqueta // Vehicles sense distintiu
 }
